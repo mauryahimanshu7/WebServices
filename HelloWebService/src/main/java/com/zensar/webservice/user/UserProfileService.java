57044fed8e1ed8953bc3fc6a618fdb0f5b42287b
Himanshu Maurya 7 <mauryahimanshu7@gmail.com>
2019-10-04 00:30:23 +0530

    added a new web service

---
+++ HelloWebService/src/main/java/com/zensar/webservice/user/UserProfileService.java
@@ -0,0 +1,21 @@
+package com.zensar.webservice.user;
+
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+@Path("/user")
+public class UserProfileService {
+
+	@POST
+	@Path("/add")
+	@Produces("text/html")
+	public Response addUser(@FormParam("clientName")String name, @FormParam("gender")String gender,
+			@FormParam("age")int age) {
+		return Response.status(200).entity("<p>new user record is added <br>name : "+name+"<br>Age : "+age+"<br>Gender : "+gender).build();
+
+	}
+
+}
